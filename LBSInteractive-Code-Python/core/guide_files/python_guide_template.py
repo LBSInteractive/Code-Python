e96f8c672100870456671c6b52e448a208bbe72f
LBSInteractive <36526361+LBSInteractive@users.noreply.github.com>
2018-02-25 18:31:22 -0500

    Archivos para Código Python
    Se puede arrancar apuntando desde CMD al exe Python 3.6.4

---
+++ LBSInteractive-Code-Python/core/guide_files/python_guide_template.py
@@ -0,0 +1,22 @@
+#miVariable = 10
+#my_int = 7
+#my_float = 1.23
+#my_bool = True
+
+
+def spam():
+  eggs = 12
+  return eggs
+        
+print (spam())
+
+#10**2 exponecial operador
+
+
+#print (my_int)
+#print (my_float)
+#print (my_bool)
+#print (miVariable)
+#print ("hello")
+
+"""Comentario Multiple"""
