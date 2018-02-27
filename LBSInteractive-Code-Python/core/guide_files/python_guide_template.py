6e42bd157314a2357cb9f88365930b3e79bad401
LBSInteractive <36526361+LBSInteractive@users.noreply.github.com>
2018-02-27 14:06:08 -0500

    Python Actualizado
    2/27/2018

--- LBSInteractive-Code-Python/core/guide_files/python_guide_template.py
+++ LBSInteractive-Code-Python/core/guide_files/python_guide_template.py
@@ -1,3 +1,8 @@
+
+"""Documentacion"""
+#Codigo
+
+
 #miVariable = 10
 #my_int = 7
 #my_float = 1.23
@@ -10,6 +15,11 @@ def spam():
         
 print (spam())
 
+total = 3.55555
+print("%.2f" % total) #Solo dos decimales
+
+#print("%.2f" % total)
+
 #10**2 exponecial operador
 
 
@@ -19,4 +29,157 @@ print (spam())
 #print (miVariable)
 #print ("hello")
 
+
+
+
+
+#Comentario de una linea
+
+
 """Comentario Multiple"""
+
+
+"""variable String"""
+#variable = "Valor String"
+
+
+
+"""Comillas en String"""
+#\'
+
+
+
+"""Una cadena String se compone de la manera, variable = "HOLA" donde el index de H=0 , O=1, L=2 y A=3"""
+#variable = "hola" [0] 
+"""Esto guardaria en variable la h"""
+
+
+"""Tamaño de cadena String"""
+#len(variable)
+
+
+"""Poner todas las letras en minuscula de la Cadena String"""
+#variable = "HOLA".lower()
+"""en la variable quedara hola"""
+
+
+"""Poner todas las letras en mayusculas de la Cadena String"""
+#variable = "hola".upper()
+"""en variable quedara HOLA"""
+
+
+"""str es un metodo que permite imprimir texto sin necesidad de las comillas"""
+#print str(hola)
+
+
+"""Concatenar una cadena String"""
+#variable = "esto" + "es" + "una concatenacion"
+
+
+"""Remplazar variables String en una cadena de String o en un print"""
+#variable1 = "hola1"
+#variable2 = "hola2"
+#print "Este es el primer string %s , este es el otro %s." % (variable1,variable2)
+"""Esto imprimira, Este es el primer string hola1 , este es el otro hola2."""
+
+
+"""Pedir entrada de teclado"""
+#variable = raw_input("Mensaje para indicar entrada")
+
+
+"""Importar clase datatime para hacer uso del tiempo"""
+#from datatime import datetime
+
+
+"""Obtener año actual"""
+#variable = datetime.now()
+
+
+"""Imprimir datatime por mes, año y dia"""
+#variable = datetime.now()
+#print variable.year
+#print variable.month
+#print variable.day
+
+
+"""Imprimir datatime por hora, minutos y segundo"""
+#variable = datetime.now()
+#print variable.hour
+#print variable.minute
+#print variable.second
+
+
+"""IF  en Python"""
+#if 15==15:
+#  print "15 es igual a 15"
+#else:
+#  print "15 no es igual a 15"
+
+
+"""Orden de operadores de comparación"""
+#!=
+#and
+#or
+
+
+"""Obtener solo un pedazo de cadena"""
+#variable = "Hola"
+#print variable[1:3]
+"""Imprime ola"""
+
+
+"""Definir una funcion"""
+#def spam():
+#  print "Eggs!"
+#
+#spam()
+"""El ultimo spam() es para invocar la funcion"""
+
+
+"""Usar sqrt en Python"""
+"""Primero importar clase math"""
+#import math
+#print math.sqrt(numero)
+
+
+"""Importar solo un modulo de una clase"""
+#from math import sqrt
+
+
+"""Importar todo un modulo de clase"""
+#from math import *
+
+
+"""Para ver los modulos disponibles en una clase"""
+#import math 
+#variable = dir(math) 
+#print variable
+
+
+"""Obtener el valor maximo"""
+#variable = max(4, 8, 15)
+"""variable ahora es 15"""
+
+
+"""Obtener el valor minimo"""
+#variable = min(4, 8, 15)
+"""variable ahora es 4"""
+
+
+"""Obtener valor absoluto"""
+#variable = abs(-20)
+"""variable ahora es 20"""
+
+
+"""Imprimir el typo de dato"""
+#print type(variable)
+
+
+
+
+	
+
+
+
+
+
